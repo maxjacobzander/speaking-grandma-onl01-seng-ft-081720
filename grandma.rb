68afb67bb8375a91aef21820130924b4c1d9db9c
Max Zander <maxjacobzander@gmail.com>
2020-08-18 15:37:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if 
-    elseifphrase == "I LOVE YOU GRANDMA!"
+    elsif phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   end
   if phrase ==
