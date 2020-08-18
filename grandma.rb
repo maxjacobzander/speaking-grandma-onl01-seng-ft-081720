341e224c008b9998e5e1909a0462217995070eb7
Max Zander <maxjacobzander@gmail.com>
2020-08-18 15:33:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   end
+  if phrase ==
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
