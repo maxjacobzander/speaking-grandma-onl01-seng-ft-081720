b985b8b617d2132df33ac9b464294cf8eecc8772
Max Zander <maxjacobzander@gmail.com>
2020-08-18 15:42:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,8 @@ def speak_to_grandma(phrase)
       "HUH?! SPEAK UP, SONNY!"
     elsif phrase == "I LOVE YOU GRANDMA!"
       "I LOVE YOU TOO PUMPKIN!"
-    elsif
+    elsif phrase == "WHAT DID YOU EAT TODAY?"
+      
   end
 end
 # Whatever you say to grandma, she should respond with
