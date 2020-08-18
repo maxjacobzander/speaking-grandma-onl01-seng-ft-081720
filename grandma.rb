4b38a54599be85e56dca4190ffd9e6e4fe1e7414
Max Zander <maxjacobzander@gmail.com>
2020-08-18 15:44:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,6 +6,7 @@ def speak_to_grandma(phrase)
       "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == "WHAT DID YOU EAT TODAY?"
       "NO, NOT SINCE 1938!"
+    elsif
   end
 end
 # Whatever you say to grandma, she should respond with
