cd9c2b5509cf8b24c70072a428fa12a530d567f3
Max Zander <maxjacobzander@gmail.com>
2020-08-18 15:39:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   if phrase != phrase.upcase
       "HUH?! SPEAK UP, SONNY!"
     elsif phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+      "I LOVE YOU TOO PUMPKIN!"
   end
   if phrase ==
 end
